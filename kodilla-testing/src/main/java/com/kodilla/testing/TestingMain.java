9ceab60e1c90f14e0f7bdaa47ba60a82fa1bddb9
Marcin <pelemistrz@gmail.com>
2024-06-17 09:42:20 +0200

    6.2 testing calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,6 +2,21 @@ package com.kodilla.testing;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Calculator calculator = new Calculator();
+       int a = 15;
+       int b = 12;
+       int resultSubstraction =3;
+       int resultAddition = 27;
+
+       if(resultAddition == calculator.add(a,b)){
+           System.out.println("Test for addition is ok");
+       } else {
+           System.out.println("Test for addition failed");
+       }
+       if(resultSubstraction == calculator.subtract(a,b)){
+           System.out.println("Test for subtraction is ok");
+       } else {
+           System.out.println("Test for subtraction failed");
+       }
     }
 }
