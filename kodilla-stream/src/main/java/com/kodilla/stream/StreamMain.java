dd5e212e181ad374678b427072523348fe943a55
Marcin <pelemistrz@gmail.com>
2024-06-20 12:43:16 +0200

    Immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,19 +1,13 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beutifier.PoemBeutifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeutifier poemBeutifier = new PoemBeutifier();
-
-        poemBeutifier.beutify("Ala ma kota", s -> s.toUpperCase());
-        poemBeutifier.beutify("Ala ma kota", s -> "ABC" + s);
-        poemBeutifier.beutify("Ala ma kota", s -> s + "ABC");
-        poemBeutifier.beutify("Ala ma kota", s -> s.toLowerCase());
-        poemBeutifier.beutify("Ala ma kota", s -> s.substring(0,s.length()/2) + " WOW "+s.substring(s.length()/2));
-
+        NumbersGenerator.generateEven(50);
     }
 }
