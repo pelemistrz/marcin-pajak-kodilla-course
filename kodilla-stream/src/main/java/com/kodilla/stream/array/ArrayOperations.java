94cdee12f658400c4721e02af280d5ec8042f3a0
Marcin <pelemistrz@gmail.com>
2024-06-23 12:48:17 +0200

    Boar Task List

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,12 +4,12 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    public static OptionalDouble getAverage(int [] numbers){
+    public static double getAverage(int [] numbers){
         IntStream.range(0,numbers.length)
                 .forEach(i->System.out.println(numbers[i]));
-        OptionalDouble average = IntStream.range(0,numbers.length)
+        double average = IntStream.range(0,numbers.length)
                 .map(i ->numbers[i])
-                .average();
+                .average().getAsDouble();
         return average;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -13,9 +13,8 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {1,2,3,4,5,6,7,8,9,10, 11,12,13,14,15,16,17,18,19,20};
 
         //When
-        OptionalDouble average = ArrayOperations.getAverage(numbers);
-        double averageDouble = average.getAsDouble();
+        double average = ArrayOperations.getAverage(numbers);
         //Then
-        assertEquals(10.5, averageDouble, 0.001);
+        assertEquals(10.5, average, 0.001);
     }
 }
