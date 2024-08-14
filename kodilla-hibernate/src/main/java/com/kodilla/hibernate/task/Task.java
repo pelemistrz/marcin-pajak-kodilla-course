689d1bf19d180fcb60aa916b92b78f7feeb8e08d
Marcin <pelemistrz@gmail.com>
2024-08-14 11:05:43 +0200

    commit

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,65 @@
+package com.kodilla.hibernate.task;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.*;
+
+@Entity
+@Table(name="TASKS")
+public class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+
+
+    public Task(String description, int duration) {
+        this.description = description;
+        this.duration = duration;
+        this.created = new Date();
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @Column(name = "CREATED")
+    @NotNull
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(name = "DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    public Task() {
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    private void setCreated(Date created) {
+        this.created = created;
+    }
+
+    private void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
